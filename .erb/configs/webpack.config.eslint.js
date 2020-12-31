512c1133cc6f56286acf7cbc538adae4bcc338bd
Larry Kluger <larry.kluger@docusign.com>
2020-12-31 14:51:47 +0200

    Fix Husky pre-commit hook
    See https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/2692#issuecomment-752836975

--- .erb/configs/webpack.config.eslint.js
+++ .erb/configs/webpack.config.eslint.js
@@ -1,4 +1,6 @@
 /* eslint import/no-unresolved: off, import/no-self-import: off */
-require('../.erb/scripts/node_modules/@babel/register');
+// require('../.erb/scripts/node_modules/@babel/register');
+require('@babel/register');
+// See https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/2692#issuecomment-752836975
 
 module.exports = require('./webpack.config.renderer.dev.babel').default;
